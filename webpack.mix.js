77e0377db853bffc92b46f7a03353872eac93b14
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-02 22:07:15 +0700

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,18 @@
-let mix = require('laravel-mix')
+let mix = require("laravel-mix");
+let path = require("path");
 
-mix.setPublicPath('dist')
-   .js('resources/js/filter.js', 'js');
+mix
+  .setPublicPath("dist")
+  .js("resources/js/filter.js", "js")
+  .vue({ version: 3 })
+  .webpackConfig({
+    externals: {
+      vue: "Vue",
+    },
+    output: {
+      uniqueName: "njimx/nova-notes-field",
+    },
+  })
+  .alias({
+    "laravel-nova": path.join(__dirname, "vendor/laravel/nova/resources/js/mixins/packages.js"),
+  });
